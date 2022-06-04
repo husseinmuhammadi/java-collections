c84efe2dd8a5644d5830b91ecae93e92ab91f34c
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-06-04 16:02:22 +0200

    Start with java collections

---
+++ init/src/main/java/com/javastudio/tutorial/collections/CollectionInitApplication.java
@@ -0,0 +1,13 @@
+package com.javastudio.tutorial.collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CollectionInitApplication {
+
+    public static final Logger logger = LoggerFactory.getLogger(CollectionInitApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("Application started!");
+    }
+}
