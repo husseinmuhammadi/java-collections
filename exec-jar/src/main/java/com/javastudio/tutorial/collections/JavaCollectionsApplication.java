60c2116d8c5f8910118ba8b4c75915dc5b5b5159
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-06-04 17:00:35 +0200

    execuatable jar file

--- exec-jar/src/main/java/com/javastudio/tutorial/collections/JavaCollectionsApplication.java
+++ exec-jar/src/main/java/com/javastudio/tutorial/collections/JavaCollectionsApplication.java
@@ -3,9 +3,9 @@ package com.javastudio.tutorial.collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CollectionInitApplication {
+public class JavaCollectionsApplication {
 
-    public static final Logger logger = LoggerFactory.getLogger(CollectionInitApplication.class);
+    public static final Logger logger = LoggerFactory.getLogger(JavaCollectionsApplication.class);
 
     public static void main(String[] args) {
         logger.info("Application started!");
---
+++ init/src/main/java/com/javastudio/tutorial/collections/JavaCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.javastudio.tutorial.collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JavaCollectionsApplication {
+
+    public static final Logger logger = LoggerFactory.getLogger(JavaCollectionsApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("Application started!");
+    }
+}
