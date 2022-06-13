d4648e1ab5b20cea50f29a194ccc7952738636ea
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-06-13 18:33:07 +0200

    Sort in Java

---
+++ sort/src/main/java/com/javastudio/tutorial/collections/Application.java
@@ -0,0 +1,13 @@
+package com.javastudio.tutorial.collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Application started!");
+    }
+}
